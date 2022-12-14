fc0e63a406c0b10152703e22504c951e2b3fe21a
AquaHippo <nannad2000@gmail.com>
2022-12-14 12:00:02 +0100

    +updated stuff
    + updated stuff in unit test

--- Testing/UnitTest1.cs
+++ Testing/UnitTest1.cs
@@ -1,13 +1,16 @@
 using Bunit;
+using Microsoft.AspNetCore.Components;
+using System;
+using Xunit; 
 
 namespace Testing
-{
+{    
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public Task CheckIfColumnExistsElseCreate()
         {
-
+            
         }
     }
 }
